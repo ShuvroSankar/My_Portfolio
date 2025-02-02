80f2cc77d2d26d7962c1488ed81bf6ac19176cc3
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-02-03 01:27:00 +0600

    header,nav, mobile_nav,page_transition

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+// components
+import Nav from "./Nav"
+import MobileNav from "./MobileNav"
+const Header = () => {
+  return <header className="py-8 xl:py-12 text-white ">
+    <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/">
+        <h1 className="text-4xl font-semibold ">Shuvro 
+            <span className="text-accent" >.</span>
+            </h1>
+        </Link>
+        {/* desktop nov */}
+        <div className="hidden xl:flex items-center gap-8">
+        <Nav/>
+        <Link href="/contact">
+          <Button className="bg-blue-900 hover:bg-blue-950">Contact Me</Button>
+        </Link>
+
+        </div>
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav/>
+        </div>
+    </div>
+  </header>
+}
+
+export default Header
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,54 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix:"",
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	container:{
+		center:true,
+		padding:"15px",
+	},
+	screens:{
+		sm: "640px",
+		md:"768px",
+		lg: "960px",
+		xl:"1200px",
+	},
+	fontFamily:{
+		primary: "var(--font-jetbrainsMono)"
+	},
+  	extend: {
+		keyframes:{
+			"accordion-down":{
+				from: {height:0},
+				to: {height: "var(--radix-accorion-content-height)"},
+			},
+			"accordion-up":{
+				from: {height:"var(--radix-accordion-content-height)"},
+				to:{height:"0"},
+			},
+		},
+		animation:{
+			"animation":{
+				"accordion-down":"accordion-down 0.2s ease-out",
+				"accordion-up":"accordion-up 0.2s ease-out",
+
+			},
+		},
+  		colors: {
+  			primary:"#1c1c22",
+			accent:{
+				DEFAULT: "#00ff99",
+				hover:"#00e187",
+			}
+  		},
+  		
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
