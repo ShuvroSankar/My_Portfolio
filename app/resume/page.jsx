333c7765cc6591947b40572379f270af15b42f74
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-02-05 00:31:16 +0600

    resume update 1

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,145 @@
 
+"use client"
+import { Description } from "@radix-ui/react-dialog"
+import {FaHtml5, FaCss3,FaJs, FaReact,FaFigma,FaNodeJs } from "react-icons/fa"
+
+import {SiTailwindcss,SiNextdotjs } from "react-icons/si"
+
+// about data
+const about = {
+  title: "About Me",
+  Description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate ex, fuga labore dicta reprehenderit fugit maiores nostrum aliquid delectus.',
+  info:[
+    {
+      fieldName: 'Name',
+      fieldValue: 'Shuvro Sankar Sen'
+    },
+    {
+      fieldName: 'Phone',
+      fieldValue: '(+880) 177 931 9010'
+    },
+    {
+      fieldName: 'Nationality',
+      fieldValue: 'Bangladeshi'
+    },
+    {
+      fieldName: 'Email',
+      fieldValue: 'shuvrosankarsen44@gmail.com'
+    },
+    {
+      fieldName: 'Freelance',
+      fieldValue: 'Available'
+    },
+    {
+      fieldName: 'Language',
+      fieldValue: 'English, Bangali'
+    },
+  ]
+}
+// experience data
+
+const experience = {
+  icon:'',
+  title: "My experience",
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  items: [
+    {
+      company: '',
+      position: '',
+      duration: ''
+    },
+    {
+      company: '',
+      position: '',
+      duration: ''
+    },
+    {
+      company: '',
+      position: '',
+      duration: ''
+    },
+  ]
+}
+const education = {
+  icon:'',
+  title: "My education",
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  items: [
+    {
+      institution: '',
+      degree: '',
+      duration: ''
+    },
+    {
+      institution: '',
+      degree: '',
+      duration: ''
+    },
+    {
+      institution: '',
+      degree: '',
+      duration: ''
+    },
+    
+  ]
+}
+
+// skills data
+
+const skills = {
+  title: "My skills",
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  skillList: [
+    {
+      icon: "",
+      name:"",
+    },
+    {
+      icon: "",
+      name:"",
+    },
+    {
+      icon: "",
+      name:"",
+    },{
+      icon: "",
+      name:"",
+    },
+  ]
+
+}
+
+
+import {Tabs, TabsContent,TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger  } from "@radix-ui/react-tooltip"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { motion } from "framer-motion"
+
 const Resume = () => {
-  return  <div>resume page</div>
+  return  <motion.div 
+  initial={{opacity:0}} 
+  animate={{
+    opacity:1,
+    transition:{
+      delay:2.4, duration:0.4, ease:"easeIn"
+    }
+  }}
+  className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+  
+  >
+    <div className="container mx-auto">
+      <Tabs>
+        <TabsList>
+          <TabsTrigger>Experience</TabsTrigger>
+          <TabsTrigger>Education</TabsTrigger>
+          <TabsTrigger>Skills</TabsTrigger>
+          <TabsTrigger>About me</TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </div>
+  </motion.div>
   
 }
 
 export default Resume
+
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,54 +1,55 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  prefix:"",
+  prefix: "",
   theme: {
-	container:{
-		center:true,
-		padding:"15px",
-	},
-	screens:{
-		sm: "640px",
-		md:"768px",
-		lg: "960px",
-		xl:"1200px",
-	},
-	fontFamily:{
-		primary: "var(--font-jetbrainsMono)"
-	},
-  	extend: {
-		keyframes:{
-			"accordion-down":{
-				from: {height:0},
-				to: {height: "var(--radix-accorion-content-height)"},
-			},
-			"accordion-up":{
-				from: {height:"var(--radix-accordion-content-height)"},
-				to:{height:"0"},
-			},
-		},
-		animation:{
-			"animation":{
-				"accordion-down":"accordion-down 0.2s ease-out",
-				"accordion-up":"accordion-up 0.2s ease-out",
-
-			},
-		},
-  		colors: {
-  			primary:"#1c1c22",
-			accent:{
-				DEFAULT: "#00ff99",
-				hover:"#00e187",
-			}
-  		},
-  		
-  	}
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)"
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" }
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 }
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        }
+      },
+    }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    addDynamicIconSelectors()
+  ]
 };
